68d17ce8107a9071404022366f7490caf760bbdb
Kalu Awa <47619821+kaluawa22@users.noreply.github.com>
2023-11-06 21:47:35 -0600

    commented out some code

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -53,7 +53,14 @@ function App() {
 // }, []);
 
   useEffect( () => {
-    setCurrentDateTime(new Date());
+    const date = new Date();
+    const options ={
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+    const formattedDate = date.toLocaleString(undefined, options);
+    setCurrentDateTime(formattedDate);
   }, []);
 
 
