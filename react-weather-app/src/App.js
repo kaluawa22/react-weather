98deb2251eabf36ef14ddd4fe9f0c129e9534505
Kalu Awa <47619821+kaluawa22@users.noreply.github.com>
2023-11-07 16:58:38 -0600

    worked on cleaning up UI

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -303,22 +303,21 @@ function App() {
 
   return (
     <div className="App">
-      <header className="d-flex justify-content-center align-items-center">
-      
-        {/* <h2>{locationData.lat}</h2> */}
-      </header>
-      {/* Weather Dashboard Component */}
-      {/* <Dashboard
-        forecastData = {forecastData}
-        // apiData = {apiData}
-        getState = {getState}
-        inputHandler ={inputHandler}
-        submitHandler = {submitHandler}
-        kelvinToF = {kelvinToF}
-        // currentDate = {currentDate}
-        // locationData = {locationData}
-        
-      /> */}
+      <header className="d-flex justify-content-center align-items-center"></header>
+      <div class="input-group mb-3">
+        <input 
+          type="text" 
+          class="form-control" 
+          placeholder="Enter A City Name" 
+          aria-label="City Name" 
+          aria-describedby="basic-addon2"
+          onChange={inputHandler}
+          value={getState}
+        />
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" onClick={submitHandler} type="button">Button</button>
+        </div>
+      </div>
       <MDBDashboard 
         forecastData = {forecastData}
         inputHandler = {inputHandler}
@@ -343,96 +342,8 @@ function App() {
       
       />
        */}
-      {/* {forecastData !== undefined ? (
-        // Render your component when myState is not undefined
-        <ForecastList 
-          forecastData={forecastData} 
-        />
-      ) : (
-        // Optionally, render a fallback component or message when myState is undefined
-        <p>forecastData is undefined</p>
-      )} */}
-
-      
-      {/* <ForecastList
-        forecastData = {forecastData}
-        kelvinToF = {kelvinToF}
-      /> */}
-
       <div className="container">
-        {/* <div className="mt-3 d-flex flex-column justify-content-center align-items-center">
-          <div class="col-auto">
-            <label for="location-name" class="col-form-label">
-              Enter Location :
-            </label>
-          </div>
-          <div class="col-auto">
-            <input
-              type="text"
-              id="location-name"
-              class="form-control"
-              onChange={inputHandler}
-              value={getState}
-            />
-          </div>
-          <button className="btn btn-primary mt-2" onClick={submitHandler}>
-            Search
-          </button>
-        </div> */}
-  
-        {/* <div className="card mt-3 mx-auto" style={{ width: '60vw' }}>
-          {apiData.main ? (
-            <div className="card-body text-center">
-              <img
-                src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
-                alt="weather status icon"
-                className="weather-icon"
-              />
-  
-              <p className="h2">
-                {kelvinToFarenheit(apiData.main.temp)}&deg; C
-              </p>
-  
-              <p className="h5">
-                <i className="fas fa-map-marker-alt"></i>{' '}
-                <strong>{apiData.name}</strong>
-              </p>
-  
-              <div className="row mt-4">
-                <div className="col-md-6">
-                  <p>
-                    <i class="fas fa-temperature-low "></i>{' '}
-                    <strong>
-                      {kelvinToFarenheit(apiData.main.temp_min)}&deg; C
-                    </strong>
-                  </p>
-                  <p>
-                    <i className="fas fa-temperature-high"></i>{' '}
-                    <strong>
-                      {kelvinToFarenheit(apiData.main.temp_max)}&deg; C
-                    </strong>
-                  </p>
-                </div>
-                <div className="col-md-6">
-                  <p>
-                    {' '}
-                    <strong>{apiData.weather[0].main}</strong>
-                  </p>
-                  <p>
-                    <strong>
-                      {' '}
-                      {countries.getName(apiData.sys.country, 'en', {
-                        select: 'official',
-                      })}
-                    </strong>
-                  </p>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <h1>Loading</h1>
-          )}
-        </div>*/}
+    
       </div> 
       <footer className="footer">
         <code>
--- react-weather-app/src/components/MDBDashboard.js
+++ react-weather-app/src/components/MDBDashboard.js
@@ -20,6 +20,10 @@ export default function Card(props) {
   
     return (
     <section className="vh-100" style={{ backgroundColor: "#C1CFEA" }}>
+
+        
+        
+        
         {props.forecastData.list ? (
             <MDBContainer className="h-100">
             <MDBInputGroup className="mb-3">
@@ -51,7 +55,7 @@ export default function Card(props) {
                     <div className="d-flex justify-content-between pb-2">
                       <div>
                         <h2 className="display-2">
-                          <strong>{props.kelvinToF(props.forecastData.list[0].main.temp)}°F</strong>
+                          <strong>{props.kelvinToF(props.forecastData.list[0].main.temp)}° F</strong>
                           {/* <strong>{props.daysForecast["2023-11-07"][0].main.humidity}</strong> */}
                         </h2>
                         <p className="text-muted mb-0">{props.forecastData.city.name}</p>
@@ -75,7 +79,7 @@ export default function Card(props) {
                         <div className="d-flex justify-content-around text-center pb-3 pt-2">
                           <div className="flex-column">
                             <p className="small">
-                              <strong>{props.kelvinToF(props.daysForecast[item][0].main.temp_max)} F</strong>
+                              <strong>{props.kelvinToF(props.daysForecast[item][0].main.temp)} F</strong>
                             </p>
                             <MDBIcon
                               fas
@@ -110,245 +114,134 @@ export default function Card(props) {
         ) : (
 
             <MDBContainer className="h-100">
-        <MDBInputGroup className="mb-3">
-            <input
-                type="text"
-                id="location-name"
-                placeholder="Location"
-                name="location"
-                className="rounded"
-                onChange={props.inputHandler}
-                value={props.getState}
-            />
-            
-            <button className="btn btn-primary mt-2" onClick={props.submitHandler}>
-                Search
-            </button>
-        </MDBInputGroup>
+              <MDBInputGroup className="mb-3">
+                  <input
+                      type="text"
+                      id="location-name"
+                      placeholder="Location"
+                      name="location"
+                      className="rounded"
+                      onChange={props.inputHandler}
+                      value={props.getState}
+                  />
+                  
+                  <button className="btn btn-primary mt-2" onClick={props.submitHandler}>
+                      Search
+                  </button>
+              </MDBInputGroup>
 
-        <MDBRow
-          className="justify-content-center align-items-center h-100"
-          style={{ color: "#282828" }}
-        >
-          <MDBCol md="9" lg="7" xl="5">
-            <MDBCard
-              className="mb-4 gradient-custom"
-              style={{ borderRadius: "25px" }}
-            >
-              <MDBCardBody className="p-4">
-                <div className="d-flex justify-content-between pb-2">
-                  <div>
-                    <h2 className="display-2">
-                      <strong>23°C</strong>
-                    </h2>
-                    <p className="text-muted mb-0">Coimbra, Portugal</p>
-                  </div>
-                  <div>
-                    <img
-                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu3.webp"
-                      width="150px"
-                    />
-                  </div>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
+              <MDBRow
+                className="justify-content-center align-items-center h-100"
+                style={{ color: "#282828" }}
+              >
+                <MDBCol md="9" lg="7" xl="5">
+                  <MDBCard
+                    className="mb-4 gradient-custom"
+                    style={{ borderRadius: "25px" }}
+                  >
+                    <MDBCardBody className="p-4">
+                      <div className="d-flex justify-content-between pb-2">
+                        <div>
+                          <h2 className="display-2">
+                            <strong>0° F</strong>
+                          </h2>
+                          <p className="text-muted mb-0">Enter A City Name</p>
+                        </div>
+                        <div>
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu3.webp"
+                            width="150px"
+                          />
+                        </div>
+                      </div>
+                    </MDBCardBody>
+                  </MDBCard>
 
-            <MDBCard className="mb-4" style={{ borderRadius: "25px" }}>
-              <MDBCardBody className="p-4">
-                <div className="d-flex justify-content-around text-center pb-3 pt-2">
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>21°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="sun"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>12:00</strong>
-                    </p>
-                    <p
-                      className="mb-0 text-muted"
-                      style={{ fontSize: ".65rem" }}
-                    >
-                      PM
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>2°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="sun"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>1:00</strong>
-                    </p>
-                    <p
-                      className="mb-0 text-muted"
-                      style={{ fontSize: ".65rem" }}
-                    >
-                      PM
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>20°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="cloud"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>2:00</strong>
-                    </p>
-                    <p
-                      className="mb-0 text-muted"
-                      style={{ fontSize: ".65rem" }}
-                    >
-                      PM
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>19°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="cloud"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>3:00</strong>
-                    </p>
-                    <p
-                      className="mb-0 text-muted"
-                      style={{ fontSize: ".65rem" }}
-                    >
-                      PM
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>18°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="cloud-showers-heavy"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>4:00</strong>
-                    </p>
-                    <p
-                      className="mb-0 text-muted"
-                      style={{ fontSize: ".65rem" }}
-                    >
-                      PM
-                    </p>
-                  </div>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
 
-            <MDBCard className="mb-4" style={{ borderRadius: "25px" }}>
-              <MDBCardBody className="p-4">
-                <div className="d-flex justify-content-around text-center pb-3 pt-2">
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>21°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="sun"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>Mon</strong>
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>20°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="sun"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>Tue</strong>
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>16°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="cloud"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>Wed</strong>
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>17°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="cloud"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>Thu</strong>
-                    </p>
-                  </div>
-                  <div className="flex-column">
-                    <p className="small">
-                      <strong>18°C</strong>
-                    </p>
-                    <MDBIcon
-                      fas
-                      icon="cloud-showers-heavy"
-                      size="2x"
-                      className="mb-3"
-                      style={{ color: "#ddd" }}
-                    />
-                    <p className="mb-0">
-                      <strong>Fri</strong>
-                    </p>
-                  </div>
-                </div>
-              </MDBCardBody>
-            </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+                  {/* <MDBCard className="mb-4" style={{ borderRadius: "25px" }}>
+                    <MDBCardBody className="p-4">
+                      <div className="d-flex justify-content-around text-center pb-3 pt-2">
+                        <div className="flex-column">
+                          <p className="small">
+                            <strong>21°C</strong>
+                          </p>
+                          <MDBIcon
+                            fas
+                            icon="sun"
+                            size="2x"
+                            className="mb-3"
+                            style={{ color: "#ddd" }}
+                          />
+                          <p className="mb-0">
+                            <strong>Mon</strong>
+                          </p>
+                        </div>
+                        <div className="flex-column">
+                          <p className="small">
+                            <strong>20°C</strong>
+                          </p>
+                          <MDBIcon
+                            fas
+                            icon="sun"
+                            size="2x"
+                            className="mb-3"
+                            style={{ color: "#ddd" }}
+                          />
+                          <p className="mb-0">
+                            <strong>Tue</strong>
+                          </p>
+                        </div>
+                        <div className="flex-column">
+                          <p className="small">
+                            <strong>16°C</strong>
+                          </p>
+                          <MDBIcon
+                            fas
+                            icon="cloud"
+                            size="2x"
+                            className="mb-3"
+                            style={{ color: "#ddd" }}
+                          />
+                          <p className="mb-0">
+                            <strong>Wed</strong>
+                          </p>
+                        </div>
+                        <div className="flex-column">
+                          <p className="small">
+                            <strong>17°C</strong>
+                          </p>
+                          <MDBIcon
+                            fas
+                            icon="cloud"
+                            size="2x"
+                            className="mb-3"
+                            style={{ color: "#ddd" }}
+                          />
+                          <p className="mb-0">
+                            <strong>Thu</strong>
+                          </p>
+                        </div>
+                        <div className="flex-column">
+                          <p className="small">
+                            <strong>18°C</strong>
+                          </p>
+                          <MDBIcon
+                            fas
+                            icon="cloud-showers-heavy"
+                            size="2x"
+                            className="mb-3"
+                            style={{ color: "#ddd" }}
+                          />
+                          <p className="mb-0">
+                            <strong>Fri</strong>
+                          </p>
+                        </div>
+                      </div>
+                    </MDBCardBody>
+                  </MDBCard> */}
+                </MDBCol>
+              </MDBRow>
+            </MDBContainer>
 
 
 
